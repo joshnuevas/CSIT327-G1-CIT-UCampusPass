ea87cfb553917b7283a1285356a1025ab8c574dc
joshnuevas <joshnuevas3065@gmail.com>
2025-10-03 12:51:16 +0800

    feature(campuspass): add templates, static config, and fix settings

--- citu_campuspass/campuspass/urls.py
+++ citu_campuspass/campuspass/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.login_view, name="login"),
-    path("register/", views.register_view, name="register"),
+    path('', views.login_view, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
 ]
--- citu_campuspass/citu_campuspass/settings.py
+++ citu_campuspass/citu_campuspass/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'citu_campuspass.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/campuspass/'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
