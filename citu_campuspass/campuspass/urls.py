bdd49eca0f9990b85e3d45cc777c59d2fe2fc9ad
joshnuevas <joshnuevas3065@gmail.com>
2025-10-04 21:01:55 +0800

    Implement backend for login, register, and dashboard with visit and notification handling

--- citu_campuspass/campuspass/urls.py
+++ citu_campuspass/campuspass/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.login_view, name='home'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path("logout/", views.logout_view, name="logout"),
+    path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
