c76a04448ef1d14a6996b39c88e5df75c5042e64
joshnuevas <joshnuevas3065@gmail.com>
2025-10-29 22:36:02 +0800

    fix: build.sh installs dependencies from repo root

--- citu_campuspass/build.sh
+++ citu_campuspass/build.sh
@@ -7,8 +7,11 @@ export DJANGO_SETTINGS_MODULE=citu_campuspass.citu_campuspass.settings
 # Install dependencies
 pip install -r ../requirements.txt
 
-# Run Django migrations
-python manage.py migrate --noinput
+# Run migrations
+python citu_campuspass/manage.py migrate
 
 # Collect static files
-python manage.py collectstatic --noinput
+python citu_campuspass/manage.py collectstatic --noinput
+
+# Start Gunicorn
+gunicorn citu_campuspass.wsgi:application --bind 0.0.0.0:$PORT
