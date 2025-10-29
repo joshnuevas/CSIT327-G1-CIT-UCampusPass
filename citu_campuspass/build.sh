00fc3a89656d952dfe4d1d1f7813e379f7bf56fc
joshnuevas <joshnuevas3065@gmail.com>
2025-10-29 22:30:21 +0800

    fix: build.sh installs dependencies from repo root

--- citu_campuspass/build.sh
+++ citu_campuspass/build.sh
@@ -5,7 +5,7 @@ set -o errexit  # Stop script on first error
 export DJANGO_SETTINGS_MODULE=citu_campuspass.citu_campuspass.settings
 
 # Install dependencies
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
 # Run Django migrations
 python manage.py migrate --noinput
