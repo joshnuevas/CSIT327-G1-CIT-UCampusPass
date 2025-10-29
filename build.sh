d375a0c71a89ae8dd30fb9b31399c6c7f0a364b3
joshnuevas <joshnuevas3065@gmail.com>
2025-10-29 22:43:21 +0800

    chore: update build.sh for correct Django settings and root paths

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@
 set -o errexit  # Stop script on first error
 
 # Set Django settings module
-export DJANGO_SETTINGS_MODULE=citu_campuspass.citu_campuspass.settings
+export DJANGO_SETTINGS_MODULE=citu_campuspass.settings
 
 # Install dependencies
-pip install -r ../requirements.txt
+pip install -r requirements.txt
 
 # Run migrations
-python citu_campuspass/manage.py migrate
+python manage.py migrate
 
 # Collect static files
-python citu_campuspass/manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Start Gunicorn
 gunicorn citu_campuspass.wsgi:application --bind 0.0.0.0:$PORT
