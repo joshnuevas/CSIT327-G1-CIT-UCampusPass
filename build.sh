7dfbd13e2815180fdb67b671fa8646ea3a407268
joshnuevas <joshnuevas3065@gmail.com>
2025-10-30 00:36:00 +0800

    fix: remove gunicorn from build.sh to prevent deploy hang

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ export DJANGO_SETTINGS_MODULE=citu_campuspass.settings
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn citu_campuspass.wsgi:application --bind 0.0.0.0:$PORT
