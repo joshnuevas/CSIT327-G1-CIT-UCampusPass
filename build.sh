9b643196bdf33e5d1b1f4c59b944b110f41c1acd
joshnuevas <joshnuevas3065@gmail.com>
2025-10-29 19:24:33 +0800

    chore: correct manage.py path in build script

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ set -o errexit  # Stop script on first error
 # Install dependencies
 pip install -r requirements.txt
 
-# Run Django migrations (inside the citu_campuspass folder)
-python citu_campuspass/manage.py migrate --noinput
+# Run Django migrations
+python manage.py migrate --noinput
 
-python citu_campuspass/manage.py collectstatic --noinput
+# Collect static files
+python manage.py collectstatic --noinput
