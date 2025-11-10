bd0aa51d9299d97d8a67f9322921490d8d67f4c2
mldyann02 <mldyann02@gmail.com>
2025-11-11 00:13:40 +0800

    feature(manage_reports_logs_app): admin-reports

--- manage_reports_logs_app/views.py
+++ manage_reports_logs_app/views.py
@@ -1,6 +1,4 @@
-# manage_reports_logs_app/views.py
-from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.shortcuts import render
 from manage_staff_app.views import admin_required
 from . import services
 import json
@@ -8,8 +6,19 @@ import json
 @admin_required
 def logs_view(request):
     logs = services.list_logs(limit=2000)
-    # print("Fetched logs:", logs)
-
     return render(request, "manage_reports_logs_app/logs.html", {
-        "logs_json": json.dumps(logs, default=str)  # For JS use
+        "logs_json": json.dumps(logs, default=str)
+    })
+
+@admin_required
+def reports_view(request):
+    """Main Reports page — summary + charts."""
+    visits = services.list_visits(limit=2000)
+    users = services.list_users(limit=2000)
+    staff = services.list_staff(limit=1000)
+
+    return render(request, "manage_reports_logs_app/reports.html", {
+        "visits_json": json.dumps(visits, default=str),
+        "users_json": json.dumps(users, default=str),
+        "staff_json": json.dumps(staff, default=str)
     })
